b841d2187120ce0aa0d46323be28d6668aa67d2f
maruti <maruti@nexright.com>
2017-08-31 13:33:06 +0530

    creator

--- buildfiles/barCreator.bat
+++ buildfiles/barCreator.bat
@@ -1,1 +1,1 @@
-mqsicreatebar -data C:\Program Files (x86)\Jenkins\workspace\poc -b D:\technology\IIB\deployment\test1.bar -a Test
+mqsicreatebar -data "C:\Program Files (x86)\Jenkins\workspace\poc" -b D:\technology\IIB\deployment\test1.bar -a Test
